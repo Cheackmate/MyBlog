3e55f0c6aa01ce699e4287cfdb1c9e93dc5caa89
Cheackmate <907153533@qq.com>
2020-10-05 11:33:24 +0800

    2020/10/5 第五次提交 linuxnote先关程序程序完成

--- src/main/java/com/moonmagician/reloads/controller/LinuxNoteController.java
+++ src/main/java/com/moonmagician/reloads/controller/LinuxNoteController.java
@@ -5,20 +5,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 
 @Controller
 public class LinuxNoteController {
-    @RequestMapping("/linuxnote/note/{id}")
-    public void toLinuxNotePath(@PathVariable("id") int id) throws FileNotFoundException {
+
+    //@RequestMapping("/linuxnote/note/{id}")
+    public ModelAndView toLinuxNotePath(@PathVariable("id") int id) throws FileNotFoundException {
         //获取linuxnote文件夹下所有文件的名称，然后存放到一个数组中，使用id访问指定的文件
-
+        System.out.println("请求到了这里");
         //获取项目根路径
-        File path = new File(ResourceUtils.getURL("classpath:").getPath());
-        if(!path.exists()) path = new File("");
-        System.out.println("path:"+path.getAbsolutePath());
-
+//        File path = new File(ResourceUtils.getURL("classpath:").getPath());
+//        if(!path.exists()) path = new File("");
+//        System.out.println("path:"+path.getAbsolutePath());
+        ModelAndView modelAndView =new ModelAndView();
+        modelAndView.setViewName("/templates/bar/pagelist/linuxnote/Day01-Linux基础.pdf");
+        modelAndView.addObject("key", 12345);
+        return modelAndView;
     }
 }
